d26e16e28eb4a1fbd172f59672d3e690d7b2eb04
RedHair96 <pht.27071996@gmail.com>
2019-02-10 22:37:44 +0700

    set nodemon

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-;const app = express();
+const express = require('express');
+const app = express();
 const port = 3000;
+
+// Midleware
+app.use(express.static('public'));
 
 // Routing
 app.get('/', (req, res) => res.send('Hello World!'));
